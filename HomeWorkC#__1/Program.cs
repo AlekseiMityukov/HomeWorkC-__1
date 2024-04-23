5b8c6350a2e3374fc69b3be009efb5895cad54d9
SimpLeLuCki <160256209+SimpLeLuCki@users.noreply.github.com>
2024-04-23 17:52:55 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkC#__1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HomeWorkC___1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Users user1 = new Users("Yejhgjg", "HJJHd123")
+            {
+                NickName = "Alexandr",
+            };
+
+            Users user2 = new Users("RBHBKK", "Rollp12");
+
+
+            Users user3 = new Users("Prosstpo", "Windw3")
+            {
+                NickName = "SimpleLuck",
+                Mail = "LuckyMan@gmail.com"
+            };
+
+            Users user4 = new Users("Fagset", "Rom213")
+            {
+                Mail = "Paradox@mail.ru"
+            };
+
+
+            Console.WriteLine($"Логин:{user1.Login} Пароль:{user1.Password}");
+            
+
+        }
+    }
+}
---
+++ HomeWorkC#__1/Users.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HomeWorkC___1
+{
+
+    internal class Users 
+    {
+        internal string login; 
+        internal string Login 
+        {
+            get { return login; }
+            set 
+            {
+                if (value.Length < 3)
+                {
+                    Console.WriteLine("Логин слишком короткий");
+                }
+                else if (value.Length > 12) 
+                {
+                    Console.WriteLine("Логин слишком длинный");
+                }
+                else
+                {
+                    login = value;
+                }
+            } 
+        }
+
+        internal string password;
+        internal string Password 
+        {
+            get { return password; }
+            set
+            { 
+                bool CorrectDigit = value.Any(char.IsDigit);
+                bool CorrectLetter = value.Any(char.IsLetter);
+                if (CorrectDigit && CorrectLetter)
+                {
+                    password = value;
+                }
+                else 
+                {
+                    Console.WriteLine("В пароле должна быть хотя бы одна цифра, буква");
+                }
+            } 
+        }
+
+
+
+        internal string Mail { get; set; }
+
+        internal string NickName { get; set; }
+
+        public Users(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+    }
+
+
+
+    
+}
